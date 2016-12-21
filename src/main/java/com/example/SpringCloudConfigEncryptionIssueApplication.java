49f96a87297b5a124715092779257f250333aa51
Fitzgerald, Andrew <andrewcfitzgerald@gmail.com>
2016-12-21 00:32:30 -0500

    initial commit from spring initializer

---
+++ src/main/java/com/example/SpringCloudConfigEncryptionIssueApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigEncryptionIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigEncryptionIssueApplication.class, args);
+	}
+}
